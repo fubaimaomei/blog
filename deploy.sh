31f3b02e1e88bc75f849f5cb4eb57a207c54e44c
凡凡 <fubaimaomei@sina.com>
2019-07-26 17:28:22 +0800

    修改函数基础-总结

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # https://github.com/fubaimaomei/blog.git
-git push -f https://github.com/fubaimaomei/blog.git master:gh-pages
+git push -f https://github.com/fubaimaomei/fubaimaomei.github.io.git master
 
 cd -
 
